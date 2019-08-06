411ac039ac4bd927244a53c879c19db2cd0b9665
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-05 23:00:25 -0600

    [UPDATED] added verificacion

--- dashboard/verificacion.php
+++ dashboard/verificacion.php
@@ -1,21 +1,24 @@
+<?php
+$codigo = $_GET['codigo'];
+?>
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <link rel="apple-touch-icon" sizes="57x57" href="../Asset/img/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="../Asset/img/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="../Asset/img/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="../Asset/img/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="../Asset/img/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="../Asset/img/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="../Asset/img/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="../Asset/img/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="../Asset/img/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="../Asset/img/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../Asset/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="../Asset/img/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../Asset/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="../Asset/img/favicon/manifest.json">
+    <link rel="apple-touch-icon" sizes="57x57" href="./../Asset/img/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./../Asset/img/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./../Asset/img/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./../Asset/img/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./../Asset/img/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./../Asset/img/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./../Asset/img/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./../Asset/img/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./../Asset/img/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="./../Asset/img/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./../Asset/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./../Asset/img/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./../Asset/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="./../Asset/img/favicon/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
@@ -32,7 +35,9 @@
       
         <div id="alertModal" class="d-none alert alert-danger" >
           <button type="button" class="close" onClick="$('#alertModal').addClass('d-none');">&times;</button>
-          Ingresa tus datos correctos para continuar
+          <span id="ErrorMesagge">
+
+          </span>
             </div>
     <div class="row h-100">
         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
@@ -43,7 +48,7 @@
                 
                         <div class="row w-75 mx-auto text-light responsive-full-w responsive-mb-3 responsive-widht">
                                 <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 offset-xs-1 offset-sm-1 offset-md-1 offset-lg-1 text-right d-block responsive-text responsive-widht">
-                                    <div>Miercoles 22 de agosto</div>
+                                    <div>Miercoles 21 de agosto</div>
                                     <div>09:00 horas</div>
                                 </div>
                                 <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 text-center responsive-widht">
@@ -62,8 +67,8 @@
                                 <span>Organizado por:</span>
                             </div>
                             <div class="d-flex">
-                                    <img class="ml-auto" src="../Asset/img/Logo-5B.png">
-                                    <img  class="mr-auto" src="../Asset/img/Logo-Atefi.png">
+                                    <img class="ml-auto" src="./../Asset/img/Logo-5B.png">
+                                    <img  class="mr-auto" src="./../Asset/img/Logo-Atefi.png">
                             </div>
                         </div>
                 </div>
@@ -77,10 +82,10 @@
                              <h1 class="text-warning">Registro</h1>
                         </div>
                         <div class="form-group">
-                            <input type="name" class="form-control" id="name" name="name" placeholder="NOMBRE">
+                            <input disabled type="name" class="form-control" id="name" name="name" placeholder="NOMBRE">
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-control" id="dpi" name="dpi" placeholder="DPI / IDENTIFICACIÓN" onkeyup="cuiIsValid(event);">
+                            <input disabled type="text" class="form-control" id="dpi" name="dpi" placeholder="DPI / IDENTIFICACIÓN" onkeyup="cuiIsValid(event);">
                             
                             <div id="dpiVerificacion" class="d-none alert alert-danger w-75 mx-auto">
                                Tu Identificacion ya se encuentra registrado
@@ -89,22 +94,22 @@
                         </div>
                         <div class="form-group ">
                             <div class="row mx-auto responsive-inputs" style="width: 80%;"> 
-                                    <input type="text" class="form-control col-xs-4 col-sm-4 col-md-4 col-lg-4 " id="area" name="area" placeholder="PREFIJO" onkeyup="calidarArea(event);">
-                                    <input type="text" class="form-control col-xs-8 col-sm-8 col-md-8 col-lg-8" id="telefono" name="telefono" placeholder="TELÉFONO" onkeyup="calidarTele(event);">
+                                    <input disabled type="text" class="form-control col-xs-4 col-sm-4 col-md-4 col-lg-4 " id="area" name="area" placeholder="PREFIJO" onkeyup="calidarArea(event);">
+                                    <input disabled type="text" class="form-control col-xs-8 col-sm-8 col-md-8 col-lg-8" id="telefono" name="telefono" placeholder="TELÉFONO" onkeyup="calidarTele(event);">
                             </div>
                                     
                         </div>
                         <div class="form-group">
-                            <input type="email" class="form-control" id="email1" name="email1" placeholder="E-MAIL">
-                            <div id="emailVerificacion" class="d-none alert alert-danger mt-2 w-75 mx-auto">
+                            <input disabled type="email" class="form-control" id="email1" name="email1" placeholder="E-MAIL">
+                            <div id="email1Verificacion" class="d-none alert alert-danger mt-2 w-75 mx-auto">
                                 Tu Email ya se encuentra registrado
                              </div>
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-control" id="empresa" name="empresa" placeholder="EMPRESA" onkeyup="calidarTele(event);">
+                            <input disabled type="text" class="form-control" id="empresa" name="empresa" placeholder="EMPRESA" onkeyup="calidarTele(event);">
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-control" id="codigo" name="codigo" placeholder="CÓDIGO" onkeyup="buscaCodigo(this.value);">
+                            <input disabled type="text" class="form-control" id="codigo" name="codigo" placeholder="CÓDIGO" onkeyup="buscaCodigo(this.value);">
                             <div id="codigoVerificacion" class="d-none alert alert-danger mt-2  w-75 mx-auto">
                                 Este Código no Existe
                              </div>
@@ -113,7 +118,7 @@
                              </div>
                         </div>
                         <div class="form-group w-100 text-center">
-                            <button type="button" style="cursor:pointer;" class="btn btn-warning m-auto w-75" id="guardar">Registrar</button>
+                            <button type="button" style="cursor:pointer;" class="btn btn-success m-auto w-75" id="guardar">Registrado</button>
                         </div>
                         
                     </form>
@@ -128,7 +133,7 @@
                             </div>
 
                             <div class="text-warning">
-                                PBX: 1775 Whatsapp: 5715-1775
+                                Tel: (+502) 2420-7220
                             </div>
                         </div>
                     </div>
@@ -139,8 +144,8 @@
                 <span>Organizado por:</span>
             </div>
             <div class="d-flex">
-                    <img class="ml-auto" src="../Asset/img/Logo-5B.png">
-                    <img  class="mr-auto" src="../Asset/img/Logo-Atefi.png">
+                    <img class="ml-auto" src="./../Asset/img/Logo-5B.png">
+                    <img  class="mr-auto" src="./../Asset/img/Logo-Atefi.png">
             </div>
         </div>
     </div>
@@ -159,9 +164,10 @@
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="../Asset/js/main.js"></script>
-    <script src="../Asset/js/events.js"></script>
+    <script src="./../Asset/js/main.js"></script>
+    <script src="./../Asset/js/events.js"></script>
     <script>
+        buscarCodigo('<?php echo $codigo; ?>');
     </script>
   </body>
 </html>
